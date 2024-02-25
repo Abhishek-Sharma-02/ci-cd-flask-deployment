a708d0e218c0f4f56de42f06a0f9d0eeacded144
Abhishek-Sharma-02 <sharma.ad02@gmail.com>
2024-02-25 17:52:16 +0530

    Added appspec.yml file and corresponding deployment scripts

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -5,3 +5,5 @@ echo "This Script is used to stop already running docker container, remove them 
 sudo docker stop $(sudo docker ps -q)
 sudo docker rm $(sudo docker ps -a -q)
 sudo docker rmi $(sudo docker images -q)
+
+
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -26,3 +26,5 @@ if ! command -v aws &>/dev/null; then
     sudo apt-get install awscli -y
 fi
     echo "AWS CLI is already installed."
+
+    
